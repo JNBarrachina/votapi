aa913bf6b0d30642124da3e5fd1f5e8b32f35f53
JNBarrachina <jnb.pasadobinario@gmail.com>
2025-05-22 13:44:42 +0200

    Contabilizando votos, obteniendo resultados

--- controllers/polls.controller.js
+++ controllers/polls.controller.js
@@ -49,8 +49,29 @@ const updatePoll = async (req, res) => {
 
     const userVote = { userId: req.user._id, vote: userChoice };
 
+    const existingVote = await Poll.findOne({ _id: pollId, "votes.userId": req.user._id });
+    if (existingVote) {
+        return res.status(400).send("You have already voted in this poll.");
+    }
+
     const updatedPoll = await Poll.findByIdAndUpdate(pollId, {$push: { votes: userVote }});
     res.json(updatedPoll);
+}
+
+const closePoll = async (req, res) => {
+    if (req.user.adminRole === false) {
+        res.status(401).send("Unauthorized to close polls");
+        return;
+    }
+
+    try {
+        const pollId = await Poll.findById(req.params.id);
+        const updatedPoll = await Poll.findByIdAndUpdate(pollId, { status: "Closed"});
+        res.json(updatedPoll);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("Unexpected error");
+    }
 }
 
 const deletePoll = async (req, res) => {
@@ -58,4 +79,22 @@ const deletePoll = async (req, res) => {
     res.json(deletedPoll);
 }
 
-module.exports = { getPoll, getDashboard, createPoll, updatePoll, deletePoll }
+const getPollResults = async (req, res) => {
+    const poll = await Poll.findById(req.params.id);
+    
+    const pollResults = poll.votes.reduce((acc, vote) => {
+    if (vote.vote !== null) {
+        const optionId = vote.vote;
+        if (!acc[optionId]) {
+            acc[optionId] = 1;
+        } else {
+            acc[optionId]++;
+        }
+    }
+    return acc;
+    }, {});
+
+    res.json(pollResults);
+}
+
+module.exports = { getPoll, getDashboard, createPoll, updatePoll, closePoll, deletePoll, getPollResults };
--- routes/polls.routes.js
+++ routes/polls.routes.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const {getPoll, createPoll, updatePoll, deletePoll, getDashboard} = require("../controllers/polls.controller")
+const {getPoll, createPoll, updatePoll, deletePoll, closePoll, getDashboard, getPollResults} = require("../controllers/polls.controller")
 const router = express.Router();
 
 router.get("/:id", getPoll);
+router.get("/results/:id", getPollResults);
 router.get("/dashboard", getDashboard);
+
 router.post("/", createPoll);
-router.patch("/:id", updatePoll);
+
+router.patch("/update/:id", updatePoll);
+router.patch("/close/:id", closePoll);
+
 router.delete("/", deletePoll);
 
 module.exports = router;
